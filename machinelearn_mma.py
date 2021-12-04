b1993a5ab37a19c7b5561afcc9fb1337274b2f98
alexbalik <92473619+alexbalik@users.noreply.github.com>
2021-12-03 21:57:57 -0500

    Removed data according to identify_skew.py

--- machinelearn_mma.py
+++ machinelearn_mma.py
@@ -38,7 +38,7 @@ pd.set_option('display.max_colwidth', -1)
 
 n_5ft = pd.read_csv(r'C:\Users\irfan\mma_ml\historical_data\n_5_ft.csv')
 
-full_training = n_5ft[['a_HT', 'a_KD', 'a_body_stra', 'a_body_strl', 'a_clinch_stra', 'a_clinch_strl', 'a_ctrl_time', 'a_dist_stra', 'a_dist_strl', 'a_ground_stra', 'a_ground_strl', 'a_head_stra', 'a_head_strl', 'a_is_Open-Stance', 'a_is_Orthodox', 'a_is_Sideways', 'a_is_Southpaw', 'a_is_Switch', 'a_leg_stra', 'a_leg_strl', 'a_reach', 'a_ssa', 'a_ssl', 'a_str', 'a_stra', 'a_subatt', 'a_td', 'a_tda', 'a_weight', 'a_imp_proba', 'b_HT', 'b_KD', 'b_body_stra', 'b_body_strl', 'b_clinch_stra', 'b_clinch_strl', 'b_ctrl_time', 'b_dist_stra', 'b_dist_strl', 'b_ground_stra', 'b_ground_strl', 'b_head_stra', 'b_head_strl', 'b_imp_proba', 'b_is_Open-Stance', 'b_is_Orthodox', 'b_is_Sideways', 'b_is_Southpaw', 'b_is_Switch', 'b_leg_stra', 'b_leg_strl', 'b_reach', 'b_ssa', 'b_ssl', 'b_str', 'b_stra', 'b_subatt', 'b_td', 'b_tda', 'b_weight', 'stand_up_time', 'vegas_brier', '5_rf']]
+full_training = n_5ft[['a_HT', 'a_KD', 'a_body_stra', 'a_body_strl', 'a_clinch_stra', 'a_clinch_strl', 'a_ctrl_time', 'a_dist_stra', 'a_dist_strl', 'a_ground_stra', 'a_ground_strl', 'a_head_stra', 'a_head_strl', 'a_is_Open-Stance', 'a_is_Orthodox', 'a_is_Sideways', 'a_is_Southpaw', 'a_is_Switch', 'a_leg_stra', 'a_leg_strl', 'a_reach', 'a_ssa', 'a_ssl', 'a_str', 'a_stra', 'a_subatt', 'a_td', 'a_tda', 'a_weight', 'a_imp_proba', 'b_HT', 'b_KD', 'b_body_stra', 'b_body_strl', 'b_clinch_stra', 'b_clinch_strl', 'b_ctrl_time', 'b_dist_stra', 'b_dist_strl', 'b_ground_stra', 'b_ground_strl', 'b_head_stra', 'b_head_strl', 'b_imp_proba', 'b_is_Open-Stance', 'b_is_Orthodox', 'b_is_Sideways', 'b_is_Southpaw', 'b_is_Switch', 'b_leg_stra', 'b_leg_strl', 'b_reach', 'b_ssa', 'b_ssl', 'b_str', 'b_stra', 'b_subatt', 'b_td', 'b_tda', 'b_weight', '5_rf']]
 
 outcomes = n_5ft[['a_outcome']]
 
@@ -58,7 +58,4 @@ test_X = scaler.transform(test_X)
 
 logistic_regression.fit(train_X, train_y)
 predictions = logistic_regression.predict(test_X)
-accuracy_score(predictions,test_y)
-
 print(accuracy_score(predictions,test_y))
-print(full_training)
